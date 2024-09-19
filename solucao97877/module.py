fd8e465f2365a3a9be0a9b2ee02669d18a428e27
tavares-fiap <rm97877@fiap.com>
2024-09-19 15:34:18 -0300

    Renomeia funcao 'search_users_in_ranking' para 'search_specific_node'

--- solucao97877/module.py
+++ solucao97877/module.py
@@ -1,19 +1,19 @@
 from typing import Union, Dict, List
 from user import User
 
-def search_users_in_ranking(ranking_tree : dict, score : int) -> Union[Dict, List]:
+def search_specific_node(ranking_tree : dict, score : int) -> Union[Dict, List]:
     if ranking_tree == {}:
         return ranking_tree
     if ranking_tree['root'][0] > score:
-        return search_users_in_ranking(ranking_tree['left'], score)
+        return search_specific_node(ranking_tree['left'], score)
     elif ranking_tree['root'][0] < score:
-        return search_users_in_ranking(ranking_tree['right'], score)
+        return search_specific_node(ranking_tree['right'], score)
     else:
         return ranking_tree['root']
     
 def insert_user_in_ranking(ranking_tree : dict, user : User):
-    users_with_same_score = search_users_in_ranking(ranking_tree, user.highest_score)
-    # Verificação se 'search_users_in_ranking' devolveu uma lista
+    users_with_same_score = search_specific_node(ranking_tree, user.highest_score)
+    # Verificação se 'search_specific_node' devolveu uma lista
     if isinstance(users_with_same_score, list):
         for existing_user in users_with_same_score[1:]:
             if existing_user.cpf == user.cpf:
@@ -28,7 +28,7 @@ def insert_user_in_ranking(ranking_tree : dict, user : User):
     return
 
 def delete_user_from_ranking(ranking_tree : dict, user : User):
-    users_with_same_score = search_users_in_ranking(ranking_tree, user.highest_score)
+    users_with_same_score = search_specific_node(ranking_tree, user.highest_score)
     if isinstance(users_with_same_score, list) and len(users_with_same_score) > 1:
         for i in range(1, len(users_with_same_score)):  # Pular o primeiro elemento (que é o score)
             if users_with_same_score[i].cpf == user.cpf:
